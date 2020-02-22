d80e713840eb8f290ffe267157b9c0c119c7f20b
林泽宏 <lin.zehong@iwhalecloud.com>
2020-02-22 21:55:21 +0800

    新增 bin 命令；优化代码

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# tpl
-创建统一模块模板
+## tpl
+ 创建统一功能模块模板。
+
+ 结构如下：
+ <pre>
+xxx                     // 业务组件根目录
+  |-- components        // 业务组件目录
+  |-- models            // 业务 models 目录
+    |-- xxx.js          // 业务 model 文件，自动加载 services
+  |-- services          // 业务 services 目录
+    |-- xxx.js          // 业务 service 文件
+  |-- XxxPage.js        // 业务组件开发入口，自动 connect model
+  |-- XxxPage.less      // 业务样式
+  |-- index.js          // 业务入口，使用 dynamic 引用
+  |-- MapProps.js       // mapStateToProps、mapDispatchToProps，自动引用 model
+</pre>
+
+## 安装
+```
+npm i zh-tpl -g
+```
+
+## 使用
+```
+$ tpl 模块名
+```
+
+例如：需要创建 home 模块，则运行如下命令
+```
+tpl home
+
+```
+
+运行结果：
+<pre>
+home                    // 业务组件根目录
+  |-- components        // 业务组件目录
+  |-- models            // 业务 models 目录
+    |-- home.js         // 业务 model 文件，自动加载 services
+  |-- services          // 业务 services 目录
+    |-- home.js         // 业务 service 文件
+  |-- HomePage.js       // 业务组件开发入口，自动 connect model
+  |-- HomePage.less     // 业务样式
+  |-- index.js          // 业务入口，使用 dynamic 引用
+  |-- MapProps.js       // mapStateToProps、mapDispatchToProps，自动引用 model
+</pre>
+
+## 说明
+
+- 1.该模板是针对使用 umi 框架的项目，创建统一功能模块模板；
+
+- 2.安装完成之后，直接在项目的根目录 cmd 下运行命令（VSCode 直接打开终端运行命令）；
+
+- 3.由于实际项目开发，模块一般都是在 `src/pages` 下创建，所以，默认创建的模块放在 `src/pages` 目录下；
+
+- 4.如果执行命令的目录下没有 `src/pages` ，则默认在执行的目录下创建模块；
---
+++ src/main.js
@@ -0,0 +1,124 @@
+const fs = require("fs");
+const path = require("path");
+const chalk = require('chalk');
+const conf = require("./config");
+const { capitalize } = require('./utils');
+const {
+  indexContent,
+  mapPropsContent,
+  pContent,
+  lessContent,
+  servicesContent,
+  modelsContent,
+} = require('./template');
+
+const DIRS = ["models", "services", "components"];
+
+function Main(argv) {
+  const { _: files } = argv;
+  if (files.length > 0) {
+    files.map(file => {
+      start(file);
+      return null;
+    })
+  } else {
+    const msg = chalk.yellow(`请输入要创建的模块名称！！！`);
+    console.info(msg); // eslint-disable-line
+  }
+}
+
+function start(file) {
+  if (file.length) {
+
+    let root = "./";
+    if (isExisDir(conf.root)) {
+      root = conf.root || "./";
+    }
+
+    // 获取 root 下面的所有文件夹
+    const pagesPath = path.resolve(process.cwd(), root);
+    const allDirs = findDirs(pagesPath);
+    if (allDirs.indexOf(file) > -1) {
+      const msg = chalk.yellow(`${file} 文件夹已经存在，请重命名输入！！！`);
+      console.info(msg); // eslint-disable-line
+      return;
+    }
+
+    const filePath = path.resolve(process.cwd(), root, file);
+    const pageName = `${capitalize(file)}Page`;
+
+    // 创建目录
+    DIRS.forEach(dirName => {
+      mkdirsSync(`${filePath}/${dirName}`);
+    });
+
+    // 创建文件，写入内容
+    // index.js
+    writeFile(`${filePath}/index.js`, indexContent(pageName));
+    // MapProps.js
+    writeFile(`${filePath}/MapProps.js`, mapPropsContent(pageName, file));
+    // xxxPage.js
+    writeFile(`${filePath}/${pageName}.js`, pContent(pageName));
+    // xxxPage.less
+    writeFile(`${filePath}/${pageName}.less`, lessContent());
+    // services
+    writeFile(`${filePath}/services/${file}.js`, servicesContent(file));
+    // models
+    writeFile(`${filePath}/models/${file}.js`, modelsContent(file), true);
+  }
+}
+
+// 检查某个目录是否存在
+function isExisDir(dir) {
+  try {
+    const stat = fs.statSync(dir);
+    if (stat) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+}
+
+// 获取所有子目录
+function findDirs(dirs) {
+  const dirArr = []
+  const dirArray = fs.readdirSync(dirs);
+  for (const d of dirArray) {
+    const filePath = path.resolve(dirs, d);
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      dirArr.push(d);
+    }
+  }
+  return dirArr;
+}
+
+// 递归创建目录 同步方法
+function mkdirsSync(dirname) {
+  if (fs.existsSync(dirname)) {
+    return true;
+  } else if (mkdirsSync(path.dirname(dirname))) {
+    fs.mkdirSync(dirname);
+    return true;
+  }
+}
+
+// 创建文件
+function writeFile(filename, fileContent = '', flag = false) {
+  fs.writeFile(filename, fileContent, 'utf8', (error) => {
+    if (error) {
+      const err = chalk.red(error);
+      console.info(err); // eslint-disable-line
+      return false;
+    }
+    if (flag) {
+      const msg = chalk.green("模块创建成功！！！");
+      console.info(msg); // eslint-disable-line
+      return true;
+    }
+  })
+}
+
+module.exports = Main;
